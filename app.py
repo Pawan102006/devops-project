43d639ee7761cb14af10166880535bbb4e17e091
Pawan Kumawat <pawankumawat102006@gmail.com>
2025-07-14 03:25:34 +0530

    first commit

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 app = Flask (__name__)
 @app.route("/")
 def flaskinfo():
-    return "this is a flask file made in app.pyzgxxxfdnhcyhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh "
+    return """this is a flask file made in app.pyzgxxxfdnhcyhhhhhhhh
+ggjcgjgj
+kbjkhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh """
 if __name__=="__main__":
     app.run(host="0.0.0.0" , port="5000")
